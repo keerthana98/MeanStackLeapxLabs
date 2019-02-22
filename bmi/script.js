8cab6bbcb6087144aee9f57d1cd8934febec819a
keerthana velilani <keerthanascience@gmail.com>
2019-02-22 22:27:32 +0530

    first assignment

---
+++ bmi/script.js
@@ -0,0 +1,16 @@
+var johnM=58;
+var johnH=165;
+var MarkM=76;
+var MarkH=176;
+
+var bmiJohn=johnM/(johnH*johnH);
+var bmiMark=MarkM/(MarkH*MarkH);
+
+console.log('john bmi:'+bmiJohn);
+console.log('mark bmi:'+bmiMark);
+
+var f=bmiJohn>bmiMark;
+
+ console.log(f);
+ 
+  
