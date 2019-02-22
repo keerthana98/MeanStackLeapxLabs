1425637ebb8576bbd25b97d0de448fbded22d87a
keerthana velilani <keerthanascience@gmail.com>
2019-02-22 19:20:52 +0530

    second assignment

---
+++ JSBASICS/script.js
@@ -0,0 +1,30 @@
+function calcBill(bill)
+{
+    if(bill<50)
+    {
+         tip=0.2*bill;
+    }
+    if(bill>=50 && bill<200)
+    {
+        tip=0.15*bill;
+    }
+    if(bill>200)
+    {
+         tip=0.1*bill;
+    }
+    return tip;
+}
+var restaurantBill=[124,48,268];
+var tipp=[];
+var totBill=[];
+
+for(i=0;i<3;i++){
+   var v=calcBill(restaurantBill[i]);  
+   tipp.push(v);
+   totBill[i]=tipp[i]+restaurantBill[i];
+}
+
+console.log('tips given:'+tipp);
+console.log('total bill in each restaurant:'+totBill);
+
+
