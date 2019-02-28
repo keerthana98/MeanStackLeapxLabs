5900451957b408366cdd411d078247ad2bc16627
keerthana velilani <keerthanascience@gmail.com>
2019-02-28 21:11:54 +0530

    domBasics

---
+++ domBasics/script.js
@@ -0,0 +1,15 @@
+document.getElementById('hId').onmouseover=function(){
+this.style.background="red";
+
+}
+
+document.getElementById('hId').onmouseout=function(){
+    this.style.background="blue";
+    }
+
+    document.getElementById('txt').onmouseout=function(){
+        this.style.background="green";
+        }
+        document.getElementById('cb').onmouseover=function(){
+            this.style.background="green";
+            }
